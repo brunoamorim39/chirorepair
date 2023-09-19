51dc6755a5cf99da2bbd7f361bf8bcc1c13dacd0
Bruno Amorim <brunoamorim39@yahoo.com>
2023-09-19 09:00:26 -0400

    Add default of adjusters

--- js/index.js
+++ js/index.js
@@ -14,18 +14,34 @@ window.onload = function() {
             let productType = window.location.search.substring(1);
 
             let splashContainer = document.getElementById('splash-image');
-            let splashHTML = `<img class="header-image card mx-3 mt-3" src="./assets/${products[productType]['splash_image']}">`;
-            splashContainer.innerHTML += splashHTML;
-
             let productsContainer = document.getElementById('products-container');
-            for (product in products[productType]['products']) {
-                let productHTML = `
-                    <div class="col-md-3 col-6 px-5 mb-4">
-                        <img class="rounded w-100 mb-3" src="./assets/${products[productType]['products'][product]['image']}">
-                        <h5 class="text-center text-dark">${products[productType]['products'][product]['name']}</h5>
-                    </div>
-                `;
-                productsContainer.innerHTML += productHTML;
+
+            if (products[productType]) {
+                let splashHTML = `<img class="header-image card mx-3 mt-3" src="./assets/${products[productType]['splash_image']}">`;
+                splashContainer.innerHTML += splashHTML;
+                
+                for (product in products[productType]['products']) {
+                    let productHTML = `
+                        <div class="col-md-3 col-6 px-5 mb-4">
+                            <img class="rounded w-100 mb-3" src="./assets/${products[productType]['products'][product]['image']}">
+                            <h5 class="text-center text-dark">${products[productType]['products'][product]['name']}</h5>
+                        </div>
+                    `;
+                    productsContainer.innerHTML += productHTML;
+                }
+            } else {
+                let splashHTML = `<img class="header-image card mx-3 mt-3" src="./assets/P1020741-1.webp">`;
+                splashContainer.innerHTML += splashHTML;
+                
+                for (product in products['adjusting_instrument']['products']) {
+                    let productHTML = `
+                        <div class="col-md-3 col-6 px-5 mb-4">
+                            <img class="rounded w-100 mb-3" src="./assets/${products['adjusting_instrument']['products'][product]['image']}">
+                            <h5 class="text-center text-dark">${products['adjusting_instrument']['products'][product]['name']}</h5>
+                        </div>
+                    `;
+                    productsContainer.innerHTML += productHTML;
+                }
             }
         })
     });
