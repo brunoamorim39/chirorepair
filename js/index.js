0a7fea085cd65e4cdd36eebaac94e1ba7599b9a5
Bruno Amorim <brunoamorim39@yahoo.com>
2023-09-19 09:45:31 -0400

    Revert

--- js/index.js
+++ js/index.js
@@ -18,13 +18,26 @@ window.onload = function() {
 
             if (products[productType]) {
                 let splashHTML = `<img class="header-image card mx-3 mt-3" src="./assets/${products[productType]['splash_image']}">`;
-                splashContainer.innerHTML = splashHTML;
+                splashContainer.innerHTML += splashHTML;
                 
                 for (product in products[productType]['products']) {
                     let productHTML = `
                         <div class="col-md-3 col-6 px-5 mb-4">
                             <img class="rounded w-100 mb-3" src="./assets/${products[productType]['products'][product]['image']}">
                             <h5 class="text-center text-dark">${products[productType]['products'][product]['name']}</h5>
+                        </div>
+                    `;
+                    productsContainer.innerHTML += productHTML;
+                }
+            } else {
+                let splashHTML = `<img class="header-image card mx-3 mt-3" src="./assets/P1020741-1.webp">`;
+                splashContainer.innerHTML += splashHTML;
+                
+                for (product in products['adjustment_tool']['products']) {
+                    let productHTML = `
+                        <div class="col-md-3 col-6 px-5 mb-4">
+                            <img class="rounded w-100 mb-3" src="./assets/${products['adjustment_tool']['products'][product]['image']}">
+                            <h5 class="text-center text-dark">${products['adjustment_tool']['products'][product]['name']}</h5>
                         </div>
                     `;
                     productsContainer.innerHTML += productHTML;
